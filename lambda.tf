c65a247ad4983685a31e0b392b8ee49332c12302
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-12 23:43:11 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,8 +1,8 @@
 provider "aws"{
   region="us-east-2"
 }
-resource "aws_iam_role" "iam_for_lmda" {
-  name = "iam_for_lmda"
+resource "aws_iam_role" "iam_for_lambda1" {
+  name = "iam_for_lambda1"
 
   assume_role_policy = <<EOF
 {
@@ -20,3 +20,11 @@ resource "aws_iam_role" "iam_for_lmda" {
 }
 EOF
 }
+
+resource "aws_lambda_function" "test1_lambda" {
+  filename      = "lammbda1.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
+ source_code_hash = filebase64sha256("lammbda1.zip")
+runtime = "python3.9"
