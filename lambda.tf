e1176d39685045be8a9977dc4b982aa6bf7ba455
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-12 23:26:24 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,8 +1,8 @@
 provider "aws"{
   region="us-east-2"
 }
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+resource "aws_iam_role" "iam_for_lmda" {
+  name = "iam_for_lmda"
 
   assume_role_policy = <<EOF
 {
