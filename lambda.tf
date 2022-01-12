5eab04243aeb7e0e606addbd1c83b6b1a498c0d8
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-12 23:17:21 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -26,6 +26,5 @@ resource "aws_lambda_function" "test_lambda" {
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
- source_code_hash = filebase64sha256("lambda_function_payload.zip")
-runtime = "python3.9"
+  runtime = "python3.9"
 }
